95719e1ad47ad175ae485aa3276e07e45d6dd804
Michał Łopaciuch <michal.lopaciuch@antrez.pl>
2022-03-18 18:03:59 +0100

    Setup Boost.log library instead of own implementation

--- apps/main.in.cc
+++ apps/main.in.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "core.h"
-#include "logger.h"
+#include <boost/log/trivial.hpp>
 
 #define NO_PARAMETERS 1
 
@@ -11,17 +11,20 @@ int main(int argc, char* argv[])
 {
     if (argc < 3 || argc > 4)
     {
-        ara::Logger::lognl("Usage: ${APP_NAME} <bind addr> <port> [conf]");
+        BOOST_LOG_TRIVIAL(error) 
+            << "Insufficent number of parameters." << std::endl 
+            << "Usage: ${APP_NAME} <bind addr> <port> [conf]";
 
         return NO_PARAMETERS;
     }
 
-    auto webservice = ara::App();
+    const auto webservice = ara::App();
 
+    BOOST_LOG_TRIVIAL(trace)
+        << "${APP_NAME} is running on " << argv[1] << " port: " << argv[2] << std::endl;
     return webservice.Run(
             argv[1],
             std::atoi(argv[2])
-        )
-;
+        );
 }
 
